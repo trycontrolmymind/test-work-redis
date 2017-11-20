8832f09bf922fbd177e047f375c989c168d16c52
Vlad Yandimirkin <vlad3600@gmail.com>
2017-11-20 22:59:12 +0300

    feat: Random phrases generator

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const config = require('./config'),
     ENV = process.env.NODE_ENV;
 const redis = require("redis"),
     client = redis.createClient(config.redisPort[ENV]);
+const lorem = require("lorem-ipsum"),
+      loremDefaults = {count: 3, units: "words"};
 
 client.on("error", function (err) {
     console.log("Error " + err);
 });
 
-client.on("connect", function (err) {
-    console.log("Error " + err);
+client.on("connect", function () {
+    console.log("Connect " + lorem(loremDefaults));
 });
 
 client.set("string key", "string val", redis.print);
